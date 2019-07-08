d1c349e31280fe6c5985fe71edb5e7b82ff39760
vivek kumar <42186030+vivek15028@users.noreply.github.com>
2019-07-09 00:20:13 +0530

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,18 +1,12 @@
 package block;
 import java.util.Date;
 
-
-
-
 public class Block {
-	
-	public String hash;
-	public String previousHash; 
-	private String data; 
-	private long timeStamp;
-	private int nonce;
-	
-	
+public String hash;
+public String previousHash; 
+private String data; 
+private long timeStamp;
+private int nonce;
 	public Block(String data,String previousHash ) {
 		this.data = data;
 		this.previousHash = previousHash;
@@ -20,9 +14,7 @@ public class Block {
 		
 		this.hash = calculateHash();
 	}
-	
-	
-	public String calculateHash() {
+        public String calculateHash() {
 		String calculatedhash = StringUtil.applySha256( 
 				previousHash +
 				Long.toString(timeStamp) +
