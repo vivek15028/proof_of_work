a4023f32921b99649cacbe120d7018abd2980584
vivek kumar <42186030+vivek15028@users.noreply.github.com>
2019-07-09 00:14:39 +0530

    Update Block.java

--- Block.java
+++ Block.java
@@ -8,20 +8,20 @@ public class Block {
 	
 	public String hash;
 	public String previousHash; 
-	private String data; //our data will be a simple message.
-	private long timeStamp; //as number of milliseconds since 1/1/1970.
+	private String data; 
+	private long timeStamp;
 	private int nonce;
 	
-	//Block Constructor.  
+	
 	public Block(String data,String previousHash ) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
 		
-		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+		this.hash = calculateHash();
 	}
 	
-	//Calculate new hash based on blocks contents
+	
 	public String calculateHash() {
 		String calculatedhash = StringUtil.applySha256( 
 				previousHash +
@@ -33,7 +33,7 @@ public class Block {
 	}
 	
 	public void mineBlock(int difficulty) {
-		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		String target = new String(new char[difficulty]).replace('\0', '0'); 
 		while(!hash.substring( 0, difficulty).equals(target)) {
 			nonce ++;
 			hash = calculateHash();
