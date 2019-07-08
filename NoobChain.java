a05955574852f251b7b9cb3c9084c6c6d6a88cda
vivek kumar <42186030+vivek15028@users.noreply.github.com>
2019-07-09 00:15:54 +0530

    Update NoobChain.java

--- NoobChain.java
+++ NoobChain.java
@@ -33,21 +33,21 @@ public class NoobChain {
 		Block previousBlock;
 		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 		
-		//loop through blockchain to check hashes:
+		
 		for(int i=1; i < blockchain.size(); i++) {
 			currentBlock = blockchain.get(i);
 			previousBlock = blockchain.get(i-1);
-			//compare registered hash and calculated hash:
+			
 			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
 				System.out.println("Current Hashes not equal");			
 				return false;
 			}
-			//compare previous hash and registered previous hash
+			
 			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
 				System.out.println("Previous Hashes not equal");
 				return false;
 			}
-			//check if hash is solved
+			
 			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
 				System.out.println("This block hasn't been mined");
 				return false;
